19743f3c8ce7fdc3319572575bb4a1ea7eab8552
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-05-19 15:06:10 +0200

    Implement security configuration and logic

--- my-notions-network/src/main/java/com/aleksandrm/mynotions/security/SecurityConfig.java
+++ my-notions-network/src/main/java/com/aleksandrm/mynotions/security/SecurityConfig.java
@@ -21,7 +21,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
-    private JwtFilter jwtAuthFilter;
+    private final JwtFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -49,5 +49,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
