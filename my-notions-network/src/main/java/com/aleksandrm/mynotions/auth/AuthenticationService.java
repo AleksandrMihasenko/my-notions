7ab71b03383dff09018ce5630e86bd9c8fe962d6
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-05-26 15:33:58 +0200

    Implement full authentication flow for new users

--- my-notions-network/src/main/java/com/aleksandrm/mynotions/auth/AuthenticationService.java
+++ my-notions-network/src/main/java/com/aleksandrm/mynotions/auth/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.aleksandrm.mynotions.auth;
 import com.aleksandrm.mynotions.email.EmailService;
 import com.aleksandrm.mynotions.email.EmailTemplateName;
 import com.aleksandrm.mynotions.role.RoleRepository;
+import com.aleksandrm.mynotions.security.JwtService;
 import com.aleksandrm.mynotions.user.Token;
 import com.aleksandrm.mynotions.user.TokenRepository;
 import com.aleksandrm.mynotions.user.User;
@@ -10,11 +11,15 @@ import com.aleksandrm.mynotions.user.UserRepository;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -26,6 +31,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final EmailService emailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
     @Value("${application.mailing.frontend.activation-url}")
     private String activationUrl;
 
@@ -84,5 +91,40 @@ public class AuthenticationService {
         }
 
         return codeBuilder.toString();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var claims = new HashMap<String, Object>();
+        var user = ((User) auth.getPrincipal());
+        claims.put("fullName", user.getFullName());
+        var jwtToken = jwtService.generateToken(claims, user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public void activateAccount(String token) throws MessagingException {
+        Token savedToken = tokenRepository.findByToken(token)
+                // TODO implement exception handler
+                .orElseThrow(() -> new RuntimeException("Invalid token"));
+
+        if (LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
+            sentValidationEmail(savedToken.getUser());
+            throw new RuntimeException("Activation token has expired. A new one has been sent to your email.");
+        }
+
+        var user = userRepository.findById(savedToken.getUser().getId())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.setEnabled(true);
+        userRepository.save(user);
+
+        savedToken.setValidatedAt(LocalDateTime.now());
+        tokenRepository.save(savedToken);
     }
 }
