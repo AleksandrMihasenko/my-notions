13d89f9d4e4f716fc452edf8f79d4575599c28fe
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-06-18 16:27:50 +0200

    Add react and styles loader

--- my-notions-static/src/index.ts
+++
@@ -1,5 +0,0 @@
-function sayHelloWorld(name: string) {
-    console.log(name);
-}
-
-sayHelloWorld('Aleks');
---
+++ my-notions-static/src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom';
+import './index.sass';
+
+render(
+    <div>Test!</div>,
+    document.getElementById('root'),
+)
