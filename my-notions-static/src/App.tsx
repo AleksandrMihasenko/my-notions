fc6e40017fe6e724bd1018346e846e4cdb273161
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-06-22 14:46:39 +0200

    Add code splitting and lazy loading

--- my-notions-static/src/App.tsx
+++ my-notions-static/src/App.tsx
@@ -1,11 +1,21 @@
-import Counter from './components/Counter';
+import { Suspense } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import './index.sass';
 
 function App() {
     return (
         <div className="App">
-            Test
-            <Counter />
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
