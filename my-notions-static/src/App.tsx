5c53eece259fc5d68e3676b11ba9c0f8f561bd7f
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-06-25 16:03:25 +0200

    Implement helper function to add custom css classes

--- my-notions-static/src/App.tsx
+++ my-notions-static/src/App.tsx
@@ -3,13 +3,14 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { useTheme } from './theme/useTheme';
+import { getClassNames } from './helpers/classNames/classNames';
 import './styles/index.scss';
 
 function App() {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={getClassNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle</button>
 
             <Link to={'/'}>Main</Link>
