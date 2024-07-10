4e0d842dfcbc4aed3a80d8ba83bcd79311dfaa5f
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-07-10 12:25:28 +0200

    Add babel in the project

--- my-notions-static/config/build/buildLoaders.ts
+++ my-notions-static/config/build/buildLoaders.ts
@@ -3,6 +3,17 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            },
+        }
+    }
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -46,6 +57,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         svgLoader,
         fileLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
