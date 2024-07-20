6e2da1076e72f1fb637660e498c0b247c716d526
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-07-20 14:20:50 +0200

    Fix theme switcher component

--- my-notions-static/src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ my-notions-static/src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,8 +3,9 @@ import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import { useTheme, Theme } from 'app/providers/ThemeProvider';
 import { getClassNames } from 'shared/lib/classNames/classNames';
 import * as classes from './ThemeSwitcher.module.scss';
-import Sun from '../icons/sun.svg'
-import Moon from '../icons/moon.svg'
+// TODO manage icons
+// import Sun from '../icons/sun.svg'
+// import Moon from '../icons/moon.svg'
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -19,7 +20,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
             className={getClassNames(classes.ThemeSwitcher, {}, [className, classes[theme]])}
             onClick={toggleTheme}
         >
-            {theme === Theme.DARK ? <Sun /> : <Moon />}
+            {theme === Theme.DARK ? '<Sun />' : '<Moon />'}
         </Button>
     );
 }
