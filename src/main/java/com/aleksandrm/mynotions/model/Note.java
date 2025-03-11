47ff65bee63ce47d21efff2ec919ae880320f377
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-03-11 16:54:00 +0100

    Fixes

--- src/main/java/com/aleksandrm/mynotions/model/Note.java
+++ src/main/java/com/aleksandrm/mynotions/model/Note.java
@@ -1,7 +1,12 @@
 package com.aleksandrm.mynotions.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Instant;
 
+@Setter
+@Getter
 public class Note {
     private int id;
     private String title;
@@ -19,15 +24,4 @@ public class Note {
         this.creationDate = creationDate;
     }
 
-    public int getId() { return id; }
-    public String getTitle() { return title; }
-    public String getContent() { return content; }
-    public String getAuthor() { return author; }
-    public Instant getCreationDate() { return creationDate; }
-
-    public void setId(int id) { this.id = id; }
-    public void setTitle(String title) { this.title = title; }
-    public void setContent(String content) { this.content = content; }
-    public void setAuthor(String author) { this.author = author; }
-    public void setCreationDate(Instant creationDate) { this.creationDate = creationDate; }
 }
--- src/main/java/com/aleksandrm/mynotions/repository/NoteRepository.java
+++ src/main/java/com/aleksandrm/mynotions/repository/NoteRepository.java
@@ -20,11 +20,13 @@ public class NoteRepository {
 
     public void createNote(Note note) {
         String sql = "INSERT INTO notes (title, content, author, creation_date) VALUES (?, ?, ?, ?)";
+
         jdbcTemplate.update(sql, note.getTitle(), note.getContent(), note.getAuthor(), Timestamp.from(note.getCreationDate()));
     }
 
     public List<Note> getAllNotes() {
         String sql = "SELECT * FROM notes";
+
         return jdbcTemplate.query(sql, (rs, rowNum) -> new Note(
                 rs.getInt("id"),
                 rs.getString("title"),
@@ -36,11 +38,13 @@ public class NoteRepository {
 
     public void updateNote(int id, Note note) {
         String sql = "UPDATE notes SET title = ?, content = ?, author = ? WHERE id = ?";
+
         jdbcTemplate.update(sql, note.getTitle(), note.getContent(), note.getAuthor(), id);
     }
 
     public void deleteNote(int id) {
         String sql = "DELETE FROM notes WHERE id = ?";
+
         jdbcTemplate.update(sql, id);
     }
 }
