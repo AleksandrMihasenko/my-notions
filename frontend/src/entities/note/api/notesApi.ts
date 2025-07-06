428470c874d7cc536e8233dc8b48c57afab6ec60
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-07-06 14:17:47 +0200

    Add react

--- frontend/src/entities/note/api/notesApi.ts
+++
@@ -1,8 +0,0 @@
-import { baseFetch } from '@/shared/api/baseFetch';
-
-export const fetchNotes = () => baseFetch('/api/notes');
-export const createNote = (note: { title: string; content: string; author: string }) =>
-	baseFetch('/api/notes', {
-		method: 'POST',
-		body: JSON.stringify(note),
-	});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  define: {
-    'process.env': process.env
-  }
-});
+  plugins: [react()],
+})
