6af23273c45275696c847ea186704f12e04961b5
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-07-26 13:24:16 +0200

    Update frontend building

--- frontend/src/pages/NoteListPage/index.tsx
+++ frontend/src/pages/NoteListPage/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
+import classes from './test.module.css';
 
 export const NoteListPage = () => {
-	return <div>Welcome to Notion Clone</div>
+	return <div className={classes.text}>Welcome to Notion Clone</div>
 }
