3922f645087269445634b40dc6fb5c1c248c2375
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-08-02 15:57:36 +0200

    Adjust styles and implement theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,17 @@ import { Routes, Route, Link } from 'react-router-dom';
 import NoteListPage from '@/pages/NoteListPage/NoteListPage.tsx'
 import MainPage from '@/pages/MainPage/MainPage.tsx';
 import { Suspense } from 'react';
+import '@/styles/index.scss'
+import { useTheme } from '@/theme/useTheme.ts';
 
 export const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
 	return (
-		<div className="app">
+		<div className={`app ${theme}`}>
 			<Link to={"/"}>Main</Link>
 			<Link to={"/notes"}>Notes List</Link>
+			<button onClick={toggleTheme}>Toggle theme</button>
 
 			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
