f536ae02ed01dc33bcd2a27ba403ab1f4d9a19f1
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-08-09 11:57:40 +0200

    Add classNames function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import NoteListPage from '@/pages/NoteListPage/NoteListPage.tsx'
-import MainPage from '@/pages/MainPage/MainPage.tsx';
+import NoteListPage from '@/pages/NoteListPage/NoteListPage'
+import MainPage from '@/pages/MainPage/MainPage';
 import { Suspense } from 'react';
 import '@/styles/index.scss'
-import { useTheme } from '@/theme/useTheme.ts';
+import { useTheme } from '@/theme/useTheme';
+import { classNames } from '@/helpers/classNames/classNames';
 
 export const App = () => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme as string])}>
 			<Link to={"/"}>Main</Link>
 			<Link to={"/notes"}>Notes List</Link>
 			<button onClick={toggleTheme}>Toggle theme</button>
---
+++ frontend/src/helpers/classNames/classNames.ts
@@ -0,0 +1,14 @@
+type Mods = Record<string, boolean | string>;
+
+export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+	return [
+		cls,
+		...additional,
+		...Object.entries(mods)
+			.filter(([_, value]) => Boolean(value))
+			.map(([className, _]) => {
+				console.log(className);
+				return className;
+			}),
+	].join(' ')
+}
